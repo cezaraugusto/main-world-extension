285ecd51f0e3c8b1644c0f0d1e0080216f3c6591
Cezar Augusto <boss@cezaraugusto.net>
2024-06-07 14:16:40 -0300

    Covnert to ESM

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,44 @@
-const WebExtension = require('webpack-target-webextension')
-const webpack = require('webpack')
-const { join } = require('path')
+import WebExtension from "webpack-target-webextension";
+import webpack from "webpack";
+import { join } from "path";
+import { fileURLToPath } from "url";
+import {dirname} from "path";
+
+// Get the file path from the URL
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name from the file path
+const __dirname = dirname(__filename);
 
 /** @returns {webpack.Configuration} */
 const config = (a, env) => ({
-  devtool: env.mode === 'production' ? undefined : 'cheap-source-map',
+  // No eval allowed in MV3
+  devtool: "cheap-source-map",
+  devtool: env.mode === "production" ? undefined : "cheap-source-map",
   resolve: {
-    extensions: ['.js'],
+    extensions: [".js"],
   },
   entry: {
-    background: join(__dirname, './background.js'),
-    content: join(__dirname, './content.js'),
+    background: join(__dirname, "./background.js"),
+    content: join(__dirname, "./content.js"),
+  },
+  output: {
+    environment: {
+      dynamicImport: true,
+    },
   },
   plugins: [
     new WebExtension({
-      background: { serviceWorkerEntry: 'background' },
+      background: {
+        serviceWorkerEntry: "background",
+        classicLoader: true,
+      },
+      weakRuntimeCheck: false,
     }),
   ],
   devServer: {
-    hot: 'only',
+    hot: "only",
   },
-})
-module.exports = config
+});
+
+export default config;
